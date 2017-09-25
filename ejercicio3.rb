df3386e1ddada7d6b9d2dd6f20a9372fbe0bd3e1
mycamach <manoli.camacho.a@gmail.com>
2017-09-25 15:23:20 -0300

    3 ready

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -2,12 +2,23 @@ class Roulette
   def play(num)
     sample = (1..10).to_a.sample
     results_history(num, sample)
-    print num == sample ? 'win' : 'lose'
+    print num == sample ? 'Winner!' : 'Loser!', "\n"
   end
 
   def results_history(play, random)
-    File.open('roulette_history.txt', 'a') { |f| f.puts random }
-    File.open('winners.txt', 'a') {|f| f.puts play} if random == play
+    File.open('roulette_history.txt', 'a+') { |f| f.puts random }
+    File.open('winners.txt', 'a+') { |f| f.puts play } if random == play
   end
 
+  def best_bet
+    lines = File.open('roulette_history.txt', 'r').readlines
+    numbers = lines.to_a.map(&:to_i)
+    best = numbers.group_by(&:itself).values.max_by(&:size).first
+    puts "The best number to bet is #{best}."
+  end
 end
+
+bet = (1..10).to_a.sample
+
+Roulette.new.play(bet)
+Roulette.new.best_bet
